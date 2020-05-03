6ab12280166565ebeb98c16efaa1b5773a433c9a
Dominick <d3lee@ucsd.edu>
2020-05-03 01:14:54 -0700

    update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/404.js"),
+  "component---src-pages-blog-js": require("gatsby-module-loader?name=component---src-pages-blog-js!/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/Blog.js"),
   "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/index.js"),
   "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/page-2.js")
 }
@@ -10,6 +11,7 @@ exports.components = {
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/layout-index.json"),
   "404.json": require("gatsby-module-loader?name=path---404!/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/404.json"),
+  "blog.json": require("gatsby-module-loader?name=path---blog!/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/blog.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/index.json"),
   "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/page-2.json"),
   "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/404-html.json")
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.layouts = {
 
 exports.components = {
   "component---src-pages-404-js": preferDefault(require("/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/404.js")),
+  "component---src-pages-blog-js": preferDefault(require("/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/Blog.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/domogami/Documents/GitHub/domogami.github.io/src/pages/page-2.js"))
 }
@@ -15,6 +16,7 @@ exports.components = {
 exports.json = {
   "layout-index.json": require("/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/layout-index.json"),
   "404.json": require("/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/404.json"),
+  "blog.json": require("/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/blog.json"),
   "index.json": require("/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/index.json"),
   "page-2.json": require("/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/page-2.json"),
   "404-html.json": require("/Users/domogami/Documents/GitHub/domogami.github.io/.cache/json/404-html.json")
